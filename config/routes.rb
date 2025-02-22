b209c29f8851be5dfecffac7cf421fb67b4efb7f
Harsh Ambekar <hambekar@academic.rrc.ca>
2025-02-22 10:36:51 -0600

    Enhance character index page with search and filter functionality; update styles for improved layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "characters#index" # Home page
-  get "/superheroes", to: "characters#index" 
+  resources :characters, only: [ :index, :show ]
+  get "/superheroes", to: "characters#index"
   get "/users", to: "users#index"
   get "/about", to: "pages#about"
 end
