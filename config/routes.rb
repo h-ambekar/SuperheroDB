64c42d1dfbe5b9141524b263f431fba2420a1e96
Harsh Ambekar <hambekar@academic.rrc.ca>
2025-02-22 09:50:11 -0600

    Add CharactersController with index and show actions; update routes for character views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "characters/index"
+  get "characters/show"
   root "characters#index"  # Change to the correct controller & action
   get "/about", to: "pages#about"
 end
